93c82364bd8e8a695a17b0313d3be97a422e79cc
Matt Davis <matthewpeterdavis@gmail.com>
2018-12-16 10:02:10 +0200

    add basic handlebars rendering

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const countryData = require('../data.json')
 
@@ -7,8 +8,23 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+
+app.engine(
+  "hbs",
+  exphbs({
+    extname: "hbs",
+    layoutsDir: path.join(__dirname, "views", "layouts"),
+    defaultLayout: "main"
+  })
+)
+
 app.get('/', (req, res) => {
-  res.json(countryData)
+  res.render("home", {
+    title: 'Countries',
+    username: "Mavis",
+  })
 })
 
 module.exports = app
