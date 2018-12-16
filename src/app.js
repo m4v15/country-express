b1194f81d39c64b5cd9afeb79f73643aa0ab1ba0
Matt Davis <matthewpeterdavis@gmail.com>
2018-12-16 10:28:02 +0200

    use partials for header, and change header

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ app.engine(
   exphbs({
     extname: "hbs",
     layoutsDir: path.join(__dirname, "views", "layouts"),
+    partialsDir: path.join(__dirname, "views", "partials"),
     defaultLayout: "main"
   })
 )
